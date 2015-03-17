8ba9722b2830099419b2e2f1dfe610045280ca4f
Pietro Gagliardi <pietro10@mac.com>
2015-03-17 14:42:58 -0400

    More TODOs.

--- enablefocus.c
+++ enablefocus.c
@@ -3,19 +3,20 @@
 
 // TODO merge with some other files?
 
-// TODO should WM_ENABLE's handler really redraw everything?
 // TODO what happens if any of these functions fail?
 HANDLER(enableFocusHandlers)
 {
 	switch (uMsg) {
 	case WM_ENABLE:
+		// TODO figure out exactly how to redraw this properly
+		// XP changes the bg color, necessitating a full redraw
 	case WM_KILLFOCUS:
 	case WM_SETFOCUS:
 		break;
 	default:
 		return FALSE;
 	}
-	// TODO redraw only the selected row
+	// TODO redraw only the selected row on focus change
 	updateAll(t);
 	*lResult = 0;
 	return TRUE;

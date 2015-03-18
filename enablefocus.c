866c6ca195dc0c2a36ceda165318c5c4d8d1af75
Pietro Gagliardi <pietro10@mac.com>
2015-03-17 20:09:57 -0400

    More TODOs.

--- enablefocus.c
+++ enablefocus.c
@@ -10,6 +10,8 @@ HANDLER(enableFocusHandlers)
 	case WM_ENABLE:
 		// TODO figure out exactly how to redraw this properly
 		// XP changes the bg color, necessitating a full redraw
+		// TODO draw checkboxes as disabled too? listview doesn't
+		// (listview does nothing with checkboxes though, just draws the unchecked and checked states and does none of the capture stuff that we do, toggling on mouse down...)
 	case WM_KILLFOCUS:
 	case WM_SETFOCUS:
 		break;
